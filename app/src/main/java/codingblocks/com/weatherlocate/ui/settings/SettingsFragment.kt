da456adb2a9476ba45c338f1d353d1319020889e
swastikashanker <swastika.135@gmail.com>
2019-06-20 19:01:02 +0530

    Add code for shared preferences

--- app/src/main/java/codingblocks/com/weatherlocate/ui/settings/SettingsFragment.kt
+++ app/src/main/java/codingblocks/com/weatherlocate/ui/settings/SettingsFragment.kt
@@ -1,6 +1,24 @@
 package codingblocks.com.weatherlocate.ui.settings
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
 
-class SettingsFragment:Fragment() {
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+import codingblocks.com.weatherlocate.R
+
+class SettingsFragment:PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Settings"
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = null
+    }
+
+
+
+
+
 }
